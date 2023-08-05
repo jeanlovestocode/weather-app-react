ef716c38b54d3b7e20a0fec95b4e8c99d3fbcbed
jeanlovestocode <edemjeantori5@gmail.com>
2023-08-05 19:04:58 -0400

    final deploy

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -13,19 +13,20 @@ export default function WeatherIcon(props) {
     "04n": "CLOUDY",
     "09d": "RAIN",
     "09n": "RAIN",
-    "010d": "RAIN",
-    "010n": "RAIN",
-    "011d": "RAIN",
-    "011n": "RAIN",
-    "013d": "SNOW",
-    "013n": "SNOW",
-    "050d": "FOG",
-    "050n": "FOG",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
   };
+
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="5f6870"
+      color="#1e1e1e"
       size={props.size}
       animate={true}
     />
